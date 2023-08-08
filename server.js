472d4cb7f46058fdea9df999e18d6a3e70ebba3d
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2023-08-08 13:52:14 +0530

    Error handling

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 var express = require("express");
 const compression = require("compression");
-const cors = require("cors");
+// const cors = require("cors");
+const multer = require("multer");
 var bodyParser = require("body-parser");
-const sequelizeConnection = require("./src/config/database");
-
 var app = express();
 app.use(compression());
-app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+const path = require("path");
+
+const sequelizeConnection = require("./src/config/database");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // ROUTES
 const authRoutes = require("./src/routes/auth.route");
@@ -17,6 +58,13 @@ const bookRoutes = require("./src/routes/book.route");
 app.use("/", authRoutes);
 app.use("/user", userRoutes);
 app.use("/book", bookRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data, status: "FAILED" });
+});
 
 sequelizeConnection
   .sync()
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,37 +5,35 @@ const { secret, expiresIn } = require("../config/config");
 const User = require("../models/user.model"); // Import the Sequelize model
 
 // post
-exports.SIGNUP = async (request, response) => {
-  const { email, password, role } = request.body;
+exports.SIGNUP = async (request, response, next) => {
+  const { email, password, role = "admin" } = request.body;
 
   try {
     // Validate email format
     const emailRegex = /^\S+@\S+\.\S+$/;
     if (!emailRegex.test(email)) {
-      return response.status(400).send({
-        message: "Invalid email format",
-        status: "FAILED",
-      });
+      const error = new Error("Invalid email format.");
+      error.statusCode = 422;
+      throw error;
     }
 
     // Validate password requirements
     const passwordRegex =
       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,}$/;
     if (!passwordRegex.test(password)) {
-      return response.status(400).send({
-        message:
-          "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 6 characters long",
-        status: "FAILED",
-      });
+      const error = new Error(
+        "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 6 characters long."
+      );
+      error.statusCode = 422;
+      throw error;
     }
 
     // Check if email already exists
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      return response.status(409).send({
-        message: "Email already exists",
-        status: "FAILED",
-      });
+      const error = new Error("Email already exists.");
+      error.statusCode = 422;
+      throw error;
     }
 
     // Hash the password
@@ -49,32 +47,32 @@ exports.SIGNUP = async (request, response) => {
       message: "Added Successfully",
     });
   } catch (error) {
-    return response.status(500).send({
-      message: error.message,
-      status: "FAILED",
-    });
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
   }
 };
 
 // post
-exports.LOGIN = async (req, response) => {
+exports.LOGIN = async (req, response, next) => {
   const { email, password } = req.body;
 
   try {
     // Find the user in the database
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return response
-        .status(401)
-        .json({ message: "Email not found", status: "FAILED" });
+      const error = new Error("A user with this email could not be found.");
+      error.statusCode = 401;
+      throw error;
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-      return response
-        .status(401)
-        .json({ message: "Password Did not match", status: "FAILED" });
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
     }
 
     // Generate a JWT token
@@ -87,10 +85,10 @@ exports.LOGIN = async (req, response) => {
       user: user,
     });
   } catch (error) {
-    return response.status(500).send({
-      message: error.message,
-      status: "FAILED",
-    });
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
   }
 };
 
@@ -128,281 +126,3 @@ exports.GET_USER_LIST = async (request, response) => {
     });
   }
 };
-
-// router.post("/add", async (request, response) => {
-//   const { email, password, role } = request.body;
-
-//   try {
-//     // Validate email format
-//     const emailRegex = /^\S+@\S+\.\S+$/;
-//     if (!emailRegex.test(email)) {
-//       return response.status(400).send({
-//         message: "Invalid email format",
-//         status: "FAILED",
-//       });
-//     }
-
-//     // Validate password requirements
-//     const passwordRegex =
-//       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,}$/;
-//     if (!passwordRegex.test(password)) {
-//       return response.status(400).send({
-//         message:
-//           "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 6 characters long",
-//         status: "FAILED",
-//       });
-//     }
-
-//     // Check if email already exists
-//     const existingUser = await User.findOne({ where: { email } });
-//     if (existingUser) {
-//       return response.status(409).send({
-//         message: "Email already exists",
-//         status: "FAILED",
-//       });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Save the user to the database
-//     await User.create({ email, password: hashedPassword, role });
-
-//     return response.status(200).send({
-//       status: "SUCCESS",
-//       message: "Added Successfully",
-//     });
-//   } catch (error) {
-//     return response.status(500).send({
-//       message: error.message,
-//       status: "FAILED",
-//     });
-//   }
-// });
-
-// router.post("/login", async (req, response) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     // Find the user in the database
-//     const user = await User.findOne({ where: { email } });
-//     if (!user) {
-//       return response
-//         .status(401)
-//         .json({ message: "Email not found", status: "FAILED" });
-//     }
-
-//     const passwordMatch = await bcrypt.compare(password, user.password);
-
-//     if (!passwordMatch) {
-//       return response
-//         .status(401)
-//         .json({ message: "Password Did not match", status: "FAILED" });
-//     }
-
-//     // Generate a JWT token
-//     const token = jwt.sign({ userId: user.id }, secret, { expiresIn });
-
-//     // Return the token to the client
-//     response.status(200).json({
-//       token: token,
-//       status: "SUCCESS",
-//       user: user,
-//     });
-//   } catch (error) {
-//     return response.status(500).send({
-//       message: error.message,
-//       status: "FAILED",
-//     });
-//   }
-// });
-
-// router.get("/users/list", verifyToken, async (request, response) => {
-//   const { page, limit } = request.query;
-//   const pageNumber = parseInt(page) || 1;
-//   const pageSize = parseInt(limit) || 10;
-//   const startIndex = (pageNumber - 1) * pageSize;
-
-//   try {
-//     // Fetch users with pagination
-//     const users = await User.findAll({
-//       offset: startIndex,
-//       limit: pageSize,
-//     });
-
-//     // Count total number of users
-//     const totalCount = await User.count();
-//     const totalPages = Math.ceil(totalCount / pageSize);
-//     return response.status(200).send({
-//       status: "SUCCESS",
-//       data: users,
-//       pagination: {
-//         currentPage: pageNumber,
-//         pageSize: pageSize,
-//         totalCount: totalCount,
-//         totalPages: totalPages,
-//       },
-//     });
-//   } catch (error) {
-//     return response.status(500).send({
-//       message: error.message,
-//       status: "FAILED",
-//     });
-//   }
-// });
-
-// Export the router
-// module.exports = router;
-
-// Register a new user
-// router.post("/add", async (request, response) => {
-//   const { email, password } = request.body;
-
-//   // Validate email format
-//   const emailRegex = /^\S+@\S+\.\S+$/;
-//   if (!emailRegex.test(email)) {
-//     return response.status(400).send({
-//       message: "Invalid email format",
-//       status: "FAILED",
-//     });
-//   }
-
-//   // Validate password requirements
-//   const passwordRegex =
-//     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,}$/;
-//   if (!passwordRegex.test(password)) {
-//     return response.status(400).send({
-//       message:
-//         "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 6 characters long",
-//       status: "FAILED",
-//     });
-//   }
-
-//   // Check if email already exists
-//   const checkEmailSQL = "SELECT * FROM users WHERE email = ?";
-//   DATABASE.query(checkEmailSQL, [email], async (error, results) => {
-//     if (error) {
-//       return response.status(500).send({
-//         message: error.message,
-//         status: "FAILED",
-//       });
-//     }
-
-//     if (results.length > 0) {
-//       return response.status(409).send({
-//         message: "Email already exists",
-//         status: "FAILED",
-//       });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Save the user to the database
-//     const insertUserSQL = "INSERT INTO users (email, password) VALUES (?, ?)";
-//     DATABASE.query(
-//       insertUserSQL,
-//       [email, hashedPassword],
-//       async (error, results) => {
-//         if (error) {
-//           return response.status(500).send({
-//             message: error.message,
-//             status: "FAILED",
-//           });
-//         }
-//         return response.status(200).send({
-//           status: "SUCCESS",
-//           message: "Added Successfully",
-//         });
-//       }
-//     );
-//   });
-// });
-
-// Register a new user
-
-// LOGIN Generate a JWT token
-// router.post("/login", (req, response) => {
-//   const { email, password } = req.body;
-
-//   // Find the user in the database
-//   const SQL = "SELECT * FROM users WHERE email = ?";
-//   DATABASE.query(SQL, [email], async (error, results) => {
-//     if (error) {
-//       return response.status(500).send({
-//         message: error.message,
-//         status: "FAILED",
-//       });
-//     }
-
-//     if (results.length === 0) {
-//       return response
-//         .status(401)
-//         .json({ message: "Email not found", status: "FAILED" });
-//     }
-
-//     const user = results[0];
-//     const passwordMatch = await bcrypt.compare(password, user.password);
-
-//     if (!passwordMatch) {
-//       return response
-//         .status(401)
-//         .json({ message: "Password Didnot matched", status: "FAILED" });
-//     }
-
-//     // Generate a JWT token
-//     const token = jwt.sign({ userId: user.id }, secret, { expiresIn });
-
-//     // Return the token to the client
-//     response.status(200).json({
-//       token: token,
-//       status: "SUCCESS",
-//       user: user,
-//     });
-//   });
-// });
-
-// LOGIN Generate a JWT token
-
-// ALL USERS
-// router.get("/users/list", verifyToken, (request, response) => {
-//   const { page, limit } = request.query;
-//   const pageNumber = parseInt(page) || 1;
-//   const pageSize = parseInt(limit) || 10;
-//   const startIndex = (pageNumber - 1) * pageSize;
-
-//   const SQL = "SELECT * FROM users LIMIT ?, ?";
-//   DATABASE.query(SQL, [startIndex, pageSize], (error, results) => {
-//     if (error) {
-//       return response.status(500).send({
-//         message: error.message,
-//         status: "FAILED",
-//       });
-//     }
-
-//     // Count total number of users
-//     const countSQL = "SELECT COUNT(*) AS count FROM users";
-//     DATABASE.query(countSQL, (error, countResult) => {
-//       if (error) {
-//         return response.status(500).send({
-//           message: error.message,
-//           status: "FAILED",
-//         });
-//       }
-
-//       const totalCount = countResult[0].count;
-//       const totalPages = Math.ceil(totalCount / pageSize);
-
-//       return response.status(200).send({
-//         status: "SUCCESS",
-//         data: results,
-//         pagination: {
-//           currentPage: pageNumber,
-//           pageSize: pageSize,
-//           totalCount: totalCount,
-//           totalPages: totalPages,
-//         },
-//       });
-//     });
-//   });
-// });
